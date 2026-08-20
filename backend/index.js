ca8a180dd549860ce6f98dffefffc9de3e810601
DEEPIKASINGH608 <190765168+DEEPIKASINGH608@users.noreply.github.com.>
2026-08-20 17:30:34 +0530

    Read Terminal Commands

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const yargs = require('yargs');
+const { hideBin } = require('yargs/helpers');
+
+const {initRepo} = require("./controllers/init");
+
+yargs(hideBin(process.argv)).command(
+    "init",
+    "Initialise a new repository",
+    {},
+    initRepo
+);
+
+

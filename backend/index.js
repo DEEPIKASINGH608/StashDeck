defd0c738b0ac3fb869cb0e6f599d183f5980852
DEEPIKASINGH608 <190765168+DEEPIKASINGH608@users.noreply.github.com.>
2026-08-23 22:30:43 +0530

    Read Terminal Commands

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,19 @@ const yargs = require('yargs');
 const { hideBin } = require('yargs/helpers');
 
 const {initRepo} = require("./controllers/init");
+const {addRepo} = require("./controllers/add")
 
 yargs(hideBin(process.argv))
     .command("init", "Initialise a new repository", {}, initRepo)
-    .command("init", "Initialise a new repository", {}, initRepo)
+    .command("add <file>", "Add a file to repository",
+        (yargs)=>{
+            yargs.positional("file", {
+                describe: "File to add to the staging area",
+                type: "string",
+            });
+        },
+        addRepo
+    )
 
     .demandCommand(1, "You  need atleat one command")
     .help().argv;

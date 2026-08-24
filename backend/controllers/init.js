644bc332558c527154045e97497562a44f3bf5b1
DEEPIKASINGH608 <190765168+DEEPIKASINGH608@users.noreply.github.com.>
2026-08-24 21:56:46 +0530

    InitRepo Logic

--- backend/controllers/init.js
+++ backend/controllers/init.js
@@ -6,7 +6,12 @@ async function initRepo() {
     const commitsPath = path.join(repoPath, "commits");
 
     try {
-        await fs.mkdir(repoPath, { recursive: true })
+        await fs.mkdir(repoPath, { recursive: true });
+        await fs.mkdir(commitsPath, { recursive: true });
+        await fs.writeFile(
+            path.join(repoPath, "config.json"),
+            JSON.stringyfy({ bucket: process.env.S3_BUCKET })
+        );
     } catch(err) {
         console.error("Error initialising repository", err);
     }

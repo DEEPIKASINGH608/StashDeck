d9b6971f5766b9bcc1ead71ce3c78ed09812b110
DEEPIKASINGH608 <190765168+DEEPIKASINGH608@users.noreply.github.com.>
2026-08-31 22:41:03 +0530

    CommitChanges Logic

--- backend/controllers/commit.js
+++ backend/controllers/commit.js
@@ -5,7 +5,15 @@ const { v4:uuid4 } = require("uuid");
 
 async function commitRepo(message) {
     const repoPath = path.resolve(process.cwd(), ".StashDeckGit");
-    const stagedPath = path.join(repoPath, "staging")
+    const stagedPath = path.join(repoPath, "staging");
+    const commitPath = path.join(repoPath, "commits");
+
+
+    try {
+
+    } catch {
+        
+    }
 }
 
 

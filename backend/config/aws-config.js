1f008acbc47815dbc4d8a6db055157e957db15d2
DEEPIKASINGH608 <190765168+DEEPIKASINGH608@users.noreply.github.com.>
2026-09-02 20:02:36 +0530

    coNFIGURE AWS

--- backend/config/aws-config.js
+++ backend/config/aws-config.js
@@ -4,6 +4,6 @@ AWS.config.update({ region: "ap-south-1" });
 
 const s3 = new AWS.S3();
 
-const S3-BUCKET = "testbucketcodewithdeepi";
+const S3_BUCKET = "testbucketcodewithdeepi";
 
 module.exports = { s3, S3_BUCKET };
